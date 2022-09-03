652622c35ade430958548936658f124ec2e3f471
petrubear <edison.martinez@gmail.com>
2022-09-02 21:01:38 -0500

    feat: custom task parameters

--- theme-park-manager/ui/build.gradle.kts
+++ theme-park-manager/ui/build.gradle.kts
@@ -2,19 +2,32 @@ plugins {
     id("java-library")
 }
 
-abstract class FileDiffTask : DefaultTask() {
+open class FileDiffTask @Inject constructor(objects: ObjectFactory) : DefaultTask() {
+    @InputFile
+    val fileA: RegularFileProperty = objects.fileProperty()
+
+    @InputFile
+    val fileB: RegularFileProperty = objects.fileProperty()
+
     @TaskAction
     fun diff() {
-        val file1 = project.file("src/main/resources/static/images/rollercoaster.jpg")
-        val file2 = project.file("src/main/resources/static/images/logflume.jpg")
+        val f1 = fileA.orNull?.asFile
+        val f2 = fileB.orNull?.asFile
 
-        when (file1.length().compareTo(file2.length())) {
-            0 -> println("${file1.name} and ${file2.name} are the same size")
-            1 -> println("${file1.name} is bigger than ${file2.name}")
-            -1 -> println("${file2.name} is bigger than ${file1.name}")
+        f1?.let { file1 ->
+            f2?.let { file2 ->
+                when (file1.length().compareTo(file2.length())) {
+                    0 -> println("${file1.name} and ${file2.name} are the same size")
+                    1 -> println("${file1.name} is bigger than ${file2.name}")
+                    -1 -> println("${file2.name} is bigger than ${file1.name}")
+                }
+            }
         }
+
     }
 }
 
 tasks.register<FileDiffTask>("fileDiff") {
+    fileA.set(file("src/main/resources/static/images/rollercoaster.jpg"))
+    fileB.set(file("src/main/resources/static/images/logflume.jpg"))
 }
